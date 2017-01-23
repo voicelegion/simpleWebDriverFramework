b9d7b412687cf7c6a10ba7bedb2db4d636b28343
voicelegion <Romans.Pipcenko@gmail.com>
2017-01-23 17:28:56 +0200

    from Ilyas tasks completed 1-3 points.

--- webdriverFramework/src/test/java/webdriverFramework/SSMission.java
+++ webdriverFramework/src/test/java/webdriverFramework/SSMission.java
@@ -1,10 +1,51 @@
 package webdriverFramework;
 
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.server.handler.FindElement;
+import org.openqa.selenium.support.ui.ExpectedCondition;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.io.File;
+
 /**
  * Created by roman.pipchenko on 1/20/2017.
  */
 public class SSMission {
     public static void main(String[] args) {
 
+        File file = new File("C:\\tools\\Firefox\\", "firefox" + ".exe");
+        if (file.exists()) {
+            System.setProperty("webdriver.firefox.bin", "C:\\tools\\Firefox\\firefox.exe");
+        }
+
+        System.setProperty("webdriver.gecko.driver", "src\\test\\resources\\geckodriver.exe");
+        WebDriver driver = new FirefoxDriver();
+
+
+        driver.get("https://www.ss.lv");
+        driver.findElement(By.xpath("//*[@href='/ru/']")).click();
+        WebDriverWait wait = new WebDriverWait(driver, 5);
+
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@class=\"a1\"][@href=\"/ru/electronics/\"]")));
+
+        driver.findElement(By.xpath("//*[@class=\"a1\"][@href=\"/ru/electronics/\"]")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.xpath("//*[@href='/ru/electronics/search/']")));
+        driver.findElement(By.xpath("//*[@href='/ru/electronics/search/']")).click();
+        wait.until(ExpectedConditions.elementToBeClickable(By.id("ptxt")));
+        driver.findElement(By.xpath("//*[@id='ptxt']")).sendKeys("Лаптоп");
+        driver.findElement(By.xpath("//*[@id='ptxt']")).submit();
+
+        Select dropDownPodrubrika = new Select(driver.findElement(By.name("'sid'")));
+        dropDownPodrubrika.selectByValue("1");
+
+
     }
+
+
 }
