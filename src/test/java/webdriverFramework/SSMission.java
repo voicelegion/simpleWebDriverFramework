4704f404526b182deaf31d384146f7f6926add34
voicelegion <Romans.Pipcenko@gmail.com>
2017-01-24 10:36:39 +0200

    QA

--- src/test/java/webdriverFramework/SSMission.java
+++ src/test/java/webdriverFramework/SSMission.java
@@ -18,6 +18,7 @@ import java.io.File;
  * Created by roman.pipchenko on 1/20/2017.
  */
 public class SSMission {
+    
     public static void main(String[] args) {
 
         File file = new File("C:\\tools\\Firefox\\", "firefox" + ".exe");
