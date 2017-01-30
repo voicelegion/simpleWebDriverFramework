59a96b6ffac372ab9eb319577f5415c4f317265a
voicelegion <Romans.Pipcenko@gmail.com>
2017-01-30 18:18:11 +0200

    Finished ElectronicsShowSelectedPage, and few minor changes in other Pages.

--- src/main/java/pageObjects/ElectronicsShowSelectedPage.java
+++ src/main/java/pageObjects/ElectronicsShowSelectedPage.java
@@ -1,7 +1,21 @@
 package pageObjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+
+import java.util.List;
+
 /**
  * Created by roman.pipchenko on 1/30/2017.
  */
 public class ElectronicsShowSelectedPage {
+    private WebDriver driver;
+    private List<WebElement> selectedAds = driver.findElements(By.xpath("//*[contains(@id, 'tr_')][contains(@style, 'cursor')]"));
+    private String selectedAdvText = driver.findElement(By.xpath(".//a[@id and @class]")).getText();
+
+
+    public ElectronicsShowSelectedPage(WebDriver driver){
+        this.driver=driver;
+    }
 }
