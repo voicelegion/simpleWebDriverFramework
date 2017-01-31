cf5d4c5ff58c9a7402a4d14621d9fed9643c59e6
voicelegion <Romans.Pipcenko@gmail.com>
2017-01-31 19:28:20 +0200

    Refactoring Code adding FINDBY and other features of PageFactory. Done changes in HomePage, Electronis Page, ElectronicsSHowSelectedPage, ElectronicsSearchResultPage and maybe SearcElectronicsPage

--- src/main/java/pageObjects/ElectronicsShowSelectedPage.java
+++ src/main/java/pageObjects/ElectronicsShowSelectedPage.java
@@ -3,6 +3,8 @@ package pageObjects;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
 
 import java.util.List;
 
@@ -11,11 +13,14 @@ import java.util.List;
  */
 public class ElectronicsShowSelectedPage {
     private WebDriver driver;
-    public List<WebElement> selectedAds = driver.findElements(By.xpath("//*[contains(@id, 'tr_')][contains(@style, 'cursor')]"));
+    @FindBy(xpath = "//*[contains(@id, 'tr_')][contains(@style, 'cursor')]")
+    public List<WebElement> selectedAds;
+
     public String selectedAdvText = driver.findElement(By.xpath(".//a[@id and @class]")).getText();
 
 
     public ElectronicsShowSelectedPage(WebDriver driver){
         this.driver=driver;
+        PageFactory.initElements(driver,this);
     }
 }
